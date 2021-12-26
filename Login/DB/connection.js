2b9c223e5162e97ebdf26c77f4c07399025accd3
DEBAPRIYA DAS <debapriyadas73@gmail.com>
2021-12-26 13:39:22 +0530

    ..

--- Login/DB/connection.js
+++ Login/DB/connection.js
@@ -1,15 +1,10 @@
 const mongoose = require('mongoose');
 
 console.log("Connection.js");
-const URL = 'mongodb+srv://TD:tdRupi@123@cluster0.e5ng0.mongodb.net/TD?retryWrites=true&w=majority';
-mongoose.connect(URL , {
-    useNewUrlParser: true,
-    useUinifiedTopology : true ,
-    useCreateIndex:true,
-
-}).then(()=>{
+const URL = 'mongodb+srv://TD:tdRupi%40123@cluster0.e5ng0.mongodb.net/TD?retryWrites=true&w=majority';
+mongoose.connect(URL).then(()=>{
     console.log('connected')
 }).catch((e)=>{
-    console.log('no connection')
+    console.log('no connection'+ e)
 })
 
