1153a0fd36219a6f7a0be6e70b54b69c1b172fd1
DEBAPRIYA DAS <debapriyadas73@gmail.com>
2021-12-27 01:18:21 +0530

    style

--- Login/server.js
+++ Login/server.js
@@ -16,6 +16,7 @@ const getUserByEmail = require("./utils/getUserByEmail");
 const initializeLocal = require("./Auths/passportCon");
 initializeLocal(passport, (email) => getUserByEmail(email));
 
+app.use(express.static(__dirname + "/public"));
 app.set("view-engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
